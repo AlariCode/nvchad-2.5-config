3d34aa6abfe3395da762b7558cbb340e4c85ad89
AlariCode <antonlarichev@gmail.com>
2024-05-01 21:29:23 +0300

    Исправление mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -19,10 +19,10 @@ map("n", "<leader>qt", "<cmd>TodoTrouble<CR>", { desc = "Open Todo Trouble" })
 
 -- Tests
 map("n", "<leader>tt", function()
- require("neotest").run.run()
+  require("neotest").run.run()
 end, { desc = "Run nearest test" })
 map("n", "<leader>tf", function()
- require("neotest").run.run(vim.fn.expand "%")
+  require("neotest").run.run(vim.fn.expand "%")
 end, { desc = "Run file test" })
 map("n", "<leader>to", ":Neotest output<CR>", { desc = "Show test output" })
 map("n", "<leader>ts", ":Neotest summary<CR>", { desc = "Show test summary" })
@@ -49,22 +49,22 @@ map("n", "<leader>gt", ":DiffviewToggleFile<CR>", { desc = "Git File History" })
 
 -- Terminal
 map("n", "<C-]>", function()
-  require("nvchad.term").toggle({ pos = "vsp", size = 0.4 })
+  require("nvchad.term").toggle { pos = "vsp", size = 0.4 }
 end, { desc = "Toogle Terminal Vertical" })
 map("n", "<C-\\>", function()
-  require("nvchad.term").toggle({ pos = "sp", size = 0.4 })
+  require("nvchad.term").toggle { pos = "sp", size = 0.4 }
 end, { desc = "Toogle Terminal Horizontal" })
 map("n", "<C-f>", function()
-  require("nvchad.term").toggle({ pos = "float", float_opts = { width = 0.7, height = 0.7 } })
+  require("nvchad.term").toggle { pos = "float" }
 end, { desc = "Toogle Terminal Float" })
 map("t", "<C-]>", function()
-  require("nvchad.term").toggle({ pos = "vsp" })
+  require("nvchad.term").toggle { pos = "vsp" }
 end, { desc = "Toogle Terminal Vertical" })
 map("t", "<C-\\>", function()
-  require("nvchad.term").toggle({ pos = "sp" })
+  require("nvchad.term").toggle { pos = "sp" }
 end, { desc = "Toogle Terminal Horizontal" })
 map("t", "<C-f>", function()
-  require("nvchad.term").toggle({ pos = "float" })
+  require("nvchad.term").toggle { pos = "float" }
 end, { desc = "Toogle Terminal Float" })
 
 -- Basic
